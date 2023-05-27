a1e6075feb63be5308f90d28624b27bdffcf336e
Werner van der Bank <werner.bank@amcsgroup.com>
2023-05-27 16:25:50 +0100

    added base shopping list

--- trello_connector/urls.py
+++ trello_connector/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('boards/', views.boards),
     path('board/<str:id>', views.board),
+    path('shopping-list/<str:board_ids>', views.shopping_list),
     path('recipies/', views.recipies),
 ]
--- trello_connector/views.py
+++ trello_connector/views.py
@@ -13,6 +13,8 @@ def board(request, id):
     board = models.Board.get_board_details(id)
     return render(request, "board.html", {"board": board})
 
+def shopping_list(request, board_ids):
+    return render(request, "shopping-list.html")
 
 def recipies(request):
     return render(request, "recipies.html")
