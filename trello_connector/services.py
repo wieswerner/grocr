698f29882bd744186eac7d96e198f216184d6183
Werner van der Bank <werner.bank@amcsgroup.com>
2023-05-27 20:44:51 +0100

    remove stuff

--- trello_connector/services.py
+++ trello_connector/services.py
@@ -5,8 +5,8 @@ from types import SimpleNamespace
 
 class Api:
     def get_api_json(url):
-        trello_key = "ea61c22a90bf9afbacbdb0ef6d869401"
-        trello_token = "ATTA6264207f0306380a59c7f5564251d07e20a49932eac327dd45811d56b0c99cd0BB871AC9"
+        trello_key = "<key>"
+        trello_token = "<token>"
         full_url = url + "?key=" + trello_key + "&token=" + trello_token
 
         http_connection = http.client.HTTPSConnection("api.trello.com")
